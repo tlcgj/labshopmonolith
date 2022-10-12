ed48a29a4d4271b86a4733567477cb89ed96d616
Geon Jang <geon.jang@twolinecode.com>
2022-10-12 10:05:26 +0900

    start

---
+++ monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -0,0 +1,72 @@
+package labshopmonolith.domain;
+
+import labshopmonolith.domain.OrderPlaced;
+import labshopmonolith.MonolithApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Order_table")
+@Data
+
+public class Order  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String productId;
+    
+    
+    
+    
+    
+    private Integer qty;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+    }
+    @PrePersist
+    public void onPrePersist(){
+    }
+
+    public static OrderRepository repository(){
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+
+
+
+
+
+}
